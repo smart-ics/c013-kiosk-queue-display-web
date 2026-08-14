19ff0c3ed796e23752a2724cec7673ed72954856
Fikri Haikal N <74874547+fikri-enha@users.noreply.github.com>
2026-08-14 15:28:00 +0700

    fix(kiosk): remove hardcoded media caption from KioskHome.vue

--- apps/kiosk-web/src/views/KioskHome.vue
+++ apps/kiosk-web/src/views/KioskHome.vue
@@ -83,7 +83,6 @@ onUnmounted(() => {
             @ended="onVideoEnded"
             @error="onVideoError"
           />
-          <p class="kiosk-ad-caption">Media &amp; Informasi Layanan RS</p>
         </template>
         <div v-else class="kiosk-ad-media">
           <svg
@@ -99,7 +98,6 @@ onUnmounted(() => {
             <path d="M8 21h8" />
             <path d="M12 17v4" />
           </svg>
-          <p>Media &amp; Informasi Layanan RS</p>
         </div>
       </aside>
 
